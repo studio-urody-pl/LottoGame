7b5216fa99448b8aeb74cc027e433ff1a1a5137f
tumej <tumej@OPTIPLEX-WIN10>
2024-02-01 21:10:48 +0100

    Add project files.

---
+++ LottoGame/main.cpp
@@ -0,0 +1,13 @@
+#include"MainForm.h"
+
+using namespace LottoGame;
+
+[STAThreadAttribute]
+int main(array<System::String^>^ args)
+{
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+
+	Application::Run(gcnew MainForm());
+	return 0;
+}
